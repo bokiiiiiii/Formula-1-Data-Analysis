246069e6be58440765e0517d1f39a6306b032c99
bokiiiiiii <b06202035@ntu.edu.tw>
2024-06-13 19:20:24 +0800

    Add auto instagram posting fuction

--- driver_laptimes_scatterplot.py
+++ driver_laptimes_scatterplot.py
@@ -7,9 +7,11 @@ import fastf1
 import fastf1.plotting
 
 
-# @brief driver_laptimes_scatterplot: Plot driver lap times variation with pit lap annotations
-def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, race):
+QUICKLAP_THRESHOLD = 1.25
 
+# @brief driver_laptimes_scatterplot: Plot driver lap times variation with pit lap annotations
+def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, race, post: bool) -> dict:
+    
     race.load()
 
     podium_finishers = race.drivers[:2]
@@ -26,11 +28,16 @@ def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, rac
     print(stints)
     pit_lap_array = []
     legend_elements = []
+    drivers_abbr = []
+    slope_str_arrays = []
+    tire_type_arrays = []
+    pit_lap_caption_arrays = []
     for i, driver in enumerate(podium_finishers):
-        print(i)
-        driver_laps = race.laps.pick_drivers(driver).pick_quicklaps(
-            1.03
-        )  # Need adjustment
+        slope_str_array = []
+        tire_type_array = []
+        pit_lap_line_array = []
+        pit_lap_caption_array = []
+        driver_laps = race.laps.pick_drivers(driver).pick_quicklaps(QUICKLAP_THRESHOLD)
         driver_laps["LapTime(s)"] = driver_laps["LapTime"].dt.total_seconds()
 
         driver_laps = driver_laps.reset_index()
@@ -49,6 +56,7 @@ def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, rac
         )
 
         driver_abbr = race.get_driver(driver)["Abbreviation"]
+        drivers_abbr.append(driver_abbr)
         driver_name = fastf1.plotting.DRIVER_TRANSLATE[driver_abbr]
         driver_color = fastf1.plotting.DRIVER_COLORS[driver_name]
         legend_elements.append(
@@ -67,12 +75,13 @@ def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, rac
         stints_stints = stints.loc[stints["Driver"] == driver_abbr]
 
         pit_lap = 0
-        pit_lap_line_array = []
+        
         for idx, row in stints_stints.iterrows():
             pit_lap += row["LapNumber"]
             if pit_lap not in pit_lap_array:
                 pit_lap_array.append(pit_lap)
             pit_lap_line_array.append(pit_lap + 0.25 * i)
+            pit_lap_caption_array.append(f"{pit_lap}")
         if pit_lap_array:
             pit_lap_array.pop()
             pit_lap_line_array.pop()
@@ -81,7 +90,10 @@ def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, rac
             ax.axvline(x=pit_lap_line, color=driver_color, linestyle="-", linewidth=1.5)
 
         for stint in driver_laps["Stint"].unique():
+            
             stint_laps = driver_laps[driver_laps["Stint"] == stint]
+            tire_type = stint_laps["Compound"].iloc[0]
+            tire_type_array.append(tire_type)
 
             X = stint_laps["LapNumber"].values.reshape(-1, 1)
             Y = stint_laps["LapTime(s)"].values.reshape(-1, 1)
@@ -104,6 +116,7 @@ def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, rac
                 midpoint = (X.min() + X.max()) / 2 + 1
             text_y_position = reg.predict([[midpoint]])[0][0]
             slope_str = f"+{slope:.3f} s/lap" if slope > 0 else f"{slope:.3f} s/lap"
+            slope_str_array.append(slope_str.replace(' s/lap',''))
             ax.text(
                 midpoint,
                 text_y_position,
@@ -113,11 +126,14 @@ def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, rac
                 fontweight="bold",
                 verticalalignment="bottom",
             )
+        slope_str_arrays.append(slope_str_array)
+        tire_type_arrays.append(tire_type_array)
+        pit_lap_caption_arrays.append(pit_lap_caption_array)
 
     for pit_lap in pit_lap_array:
         ax.text(
             pit_lap,
-            driver_laps["LapTime(s)"].max() - 0.1,
+            driver_laps["LapTime(s)"].max() - 0.2,
             "Pit Lap",
             rotation=90,
             color="grey",
@@ -134,15 +150,20 @@ def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, rac
     ax.set_xlim(1, race.laps["LapNumber"].max())
 
     suptitle = f"{Year} {EventName} Grand Prix Driver Lap Time Variation"
-
+    
     plt.suptitle(
         suptitle,
         fontweight="bold",
         fontsize=16,
     )
-
-    subtitle = "with Lap Time Variation Rate and Pit Lap Annotated"
-    plt.figtext(0.5, 0.94, subtitle, ha="center", fontsize=14)
+    
+    subtitle_upper = "with Lap Time Variation Rate and Pit Lap Annotated"
+    subtitle_lower = f"{drivers_abbr[0]} vs {drivers_abbr[1]}"
+    bg_color = ax.get_facecolor()
+    plt.figtext(0.5, 0.935, subtitle_upper, ha="center", fontsize=14,
+            bbox=dict(facecolor=bg_color, alpha=0.5, edgecolor='none'))
+    plt.figtext(0.5, 0.912, subtitle_lower, ha="center", fontsize=12, fontweight="bold",
+            bbox=dict(facecolor=bg_color, alpha=0.5, edgecolor='none'))
 
     sns.despine(left=True, bottom=True)
 
@@ -158,3 +179,39 @@ def driver_laptimes_scatterplot(Year: int, EventName: str, SessionName: str, rac
 
     filename = "../pic/" + suptitle.replace(" ", "_") + ".png"
     plt.savefig(filename)
+    
+    titles_str = (
+        suptitle.replace(f"{Year} ", "")
+        .replace(f"{EventName} ", "")
+        .replace("Grand Prix ", "")
+    )
+    
+    print(slope_str_arrays)
+    
+    caption = f"""\
+🏎️
+« {Year} {EventName} Grand Prix »
+
+• {titles_str} {subtitle_lower}
+
+‣ Pit Lap
+\t◦ {drivers_abbr[0]}
+\t1 → {' → '.join(pit_lap_caption_arrays[0])}
+\t◦ {drivers_abbr[1]}
+\t1 → {' → '.join(pit_lap_caption_arrays[1])}    
+
+‣ Compound
+\t◦ {drivers_abbr[0]}
+\t{' → '.join(tire_type_arrays[0])}
+\t◦ {drivers_abbr[1]}
+\t{' → '.join(tire_type_arrays[1])} 
+            
+‣ Lap Time Variation Rate (s/lap)
+\t◦ {drivers_abbr[0]}
+\t{' → '.join(slope_str_arrays[0])}
+\t◦ {drivers_abbr[1]}
+\t{' → '.join(slope_str_arrays[1])}
+    
+#formula1 #{EventName.replace(" ", "")}"""
+    
+    return {"filename": filename, "caption": caption, "post": post}   
